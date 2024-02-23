816c2b128d2bd267bc5eb64516dc45759c0d026a
Nick <ngarnsworthy@gmail.com>
2024-02-22 18:00:01 -0600

    Fix name

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
-  PhotonCamera camera = new PhotonCamera("frontcamera");
+  PhotonCamera camera = new PhotonCamera("shootingSideCamera");
   
   /** Creates a new Vision. */
   public Vision() {}
